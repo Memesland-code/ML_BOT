78b9de748f036d66e2632d546fc5f97c5551b2d3
Memesland-code <memes.landytb@gmail.com>
2024-02-25 11:00:11 +0100

    Ajout d'un blocage empêchant les non administrateur d'utilser le bot - v0.0.1 (REV-5)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import DiscordJS, { GatewayIntentBits } from "discord.js"
+import DiscordJS, { ActivityType, GatewayIntentBits, GuildMember } from "discord.js"
 import WOK from 'wokcommands'
 import path from 'path'
 import dotenv from 'dotenv'
+import fs from 'fs'
 var colors = require('colors')
 dotenv.config()
 
@@ -14,6 +15,22 @@ client.on('ready', () => {
     client,
     commandsDir: path.join(__dirname, "commands"),
   })
+
+  fs.readFile('package.json', 'utf-8', function (err, data) {
+    if (err) throw err
+    const obj = JSON.parse(data)
+    const clientVersion = obj.version
+    client.user?.setActivity(`v${clientVersion}`, {type: ActivityType.Custom})
+  })
+})
+
+client.on("interactionCreate", (interaction) => {
+  const member = interaction.member as GuildMember
+  if (!member.permissions.has("Administrator") && interaction.isCommand()) {
+    interaction.reply({ content: "Ce bot n'est pas ouvert au public, vous n'avez pas l'autorisation d'exécuter de commandes", ephemeral: true })
+  } else {
+    return
+  }
 })
 
 client.login(process.env.TOKEN)
