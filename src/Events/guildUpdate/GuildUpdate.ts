0cd6e46093f855b1d0aa24c2f6fbfa770cc7e2dd
Memesland-code <memes.landytb@gmail.com>
2025-02-28 10:40:36 +0100

    Added GuildUpdate listener ; ready for Logs v1 deployment - v.0.1.0 (REV-29)

---
+++ src/Events/guildUpdate/GuildUpdate.ts
@@ -0,0 +1,35 @@
+import { AuditLogEvent, EmbedBuilder, Guild, TextChannel } from "discord.js"
+import { client } from "../../index"
+import { getLogChannel } from "../../functions"
+import colors from "colors"
+
+export default async(oldGuild: Guild, newGuild: Guild) => {
+
+    var guild = client!.guilds.cache.get(newGuild.id)
+    
+    var guildLogsChannelID = await getLogChannel(guild?.id) as string
+    var logsChannel = client.channels.cache.get(guildLogsChannelID) as TextChannel
+
+    const AuditLogFetch = await guild?.fetchAuditLogs({limit: 1, type: AuditLogEvent.GuildUpdate});
+    const Entry = AuditLogFetch?.entries.first();
+
+    console.log(colors.yellow(`EVENT\nServer was updated\n\
+    `) + colors.yellow(`Server : `) + (`${newGuild.name}\n\
+    `) + colors.yellow(`Server ID : `) + (`${newGuild.id}\n\
+    `) + colors.magenta(`Executor : `) + (`${Entry?.executor!.username}\n\
+    `) + colors.magenta(`ID : `) + (`${Entry?.executor!.id}\n\
+    `) + colors.cyan(`${new Date().toLocaleString()}\n`))
+
+    const embed = new EmbedBuilder()
+    .setAuthor({name: `${Entry?.executor?.username}`, iconURL: `${Entry?.executor?.avatarURL()}`})
+    .setTitle("Server was updated")
+    .setColor("DarkBlue")
+    .addFields([
+        {name: `Executor`, value: `\
+        User : <@${Entry?.executor?.id}>\n\
+        ID : ${Entry?.executor?.id}`}
+    ])
+    .setFooter({text: `${new Date().toLocaleString()}`})
+
+    logsChannel.send({embeds: [embed]})
+}
