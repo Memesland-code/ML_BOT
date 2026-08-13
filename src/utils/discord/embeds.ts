b531f159d1c98c82797b4af1d982d64a4e00837a
Memesland-code <memes.landytb@gmail.com>
2026-08-13 10:31:21 +0200

    feat(embeds): Modified embed builder to accept more information and optionnal information

--- src/utils/discord/embeds.ts
+++ src/utils/discord/embeds.ts
@@ -3,9 +3,11 @@ import { ColorResolvable, EmbedBuilder, PartialUser, User } from "discord.js"
 interface LogEmbedOptions
 {
     title: string
-    executor: User | PartialUser | null
-    fields: { name: string, value: string }[]
     color?: ColorResolvable
+    description?: string
+    executor?: User | PartialUser | null
+    author?: { name: string, iconURL?: string }
+    fields?: { name: string, value: string }[]
 }
 
 
@@ -15,15 +17,28 @@ export function createLogEmbed(options: LogEmbedOptions): EmbedBuilder
     const embed = new EmbedBuilder()
         .setTitle(options.title)
         .setColor(options.color ?? 'Blue')
-        .addFields(options.fields)
         .setFooter({ text: new Date().toLocaleString() })
 
-    if (options.executor)
+    // if author is explicit
+    if (options.author)
+    {
+        embed.setAuthor({
+            name: options.author.name,
+            iconURL: options.author.iconURL
+        })
+    }
+    // if no author fallback on the executor if not null
+    else if (options.executor)
     {
         embed.setAuthor({
             name: options.executor.username ?? 'Unknown User',
             iconURL: options.executor.displayAvatarURL()
         })
+    }
+
+    if (options.fields && options.fields.length > 0)
+    {
+        embed.addFields(options.fields)
     }
 
     return embed
